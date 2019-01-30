0083aba7fad84de9f345ccf0c6d29a8c58f21b54
Sara Regan <regan.sara@gmail.com>
2019-01-31 00:21:43 +0100

    Result from coding along with the the video

---
+++ scripts.js
@@ -0,0 +1,107 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+
+function getVideo () {
+	navigator.mediaDevices.getUserMedia({
+		video: true,
+		audio: false
+	})
+	.then(localMediaStream => {
+		try {
+			video.srcObject = localMediaStream
+		} catch (error) {
+			video.src = URL.createObjectURL(mediaSource)
+		}
+		
+		video.play()
+	})
+	.catch(error => {
+		console.error(`Can't access camera`, error)
+	})
+}
+
+function snapShot () {
+	const width = video.videoWidth
+	const height = video.videoHeight
+	canvas.height = height
+	canvas.width = width
+	return setInterval(() => {
+		ctx.drawImage(video, 0, 0, width, height)
+		//take the pixels
+		let pixels = ctx.getImageData(0, 0, width, height)
+		//mess with their rgb value
+		// pixels = rgbSplit(pixels)
+		// ctx.globalAlpha = 0.1;
+		greenScreen(pixels)
+		//put them back
+		ctx.putImageData(pixels, 0, 0)
+	}, 30)
+}
+
+function redEffect (pixels) {
+	for(let i = 0; i < pixels.data.length; i+=4) {
+		pixels.data[i] = pixels.data[i] + 100 //red
+		pixels.data[i + 1] = pixels.data[i + 1] - 50 //green
+		pixels.data[i + 2] = pixels.data[i + 2] * 0.5 //blue
+	}
+	return pixels
+}
+
+function rgbSplit (pixels) {
+	for(let i = 0; i < pixels.data.length; i+=4) {
+		pixels.data[i - 150] = pixels.data[i] + 100 //red
+		pixels.data[i + 500] = pixels.data[i + 1] - 50 //green
+		pixels.data[i - 550] = pixels.data[i + 2] * 0.5 //blue
+	}
+	return pixels
+}
+
+function greenScreen (pixels) {
+	const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+		}
+	}
+	return pixels
+}
+
+function takePhoto () {
+	// play sound
+	snap.currenTime = 0
+	snap.play()
+
+	//take data
+	const data = canvas.toDataURL('image/jpeg')
+	const link = document.createElement('a')
+	link.href = data
+	link.setAttribute('download', 'schoen')
+	link.textContent = 'Download Image!'
+	link.innerHTML = `<img src="${data}" alt="Cool">`
+	strip.insertBefore(link, strip.firstChild)
+}
+
+getVideo()
+
+
+video.addEventListener('canplay', snapShot)
